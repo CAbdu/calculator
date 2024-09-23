fd0430782703fd28bafc8ba9a8c48d3f91e4dd37
CAbdu <abduchaib@hotmail.fr>
2024-09-23 06:36:06 +0200

    buttons done

---
+++ calculator.js
@@ -0,0 +1,30 @@
+var numOne = document.getElementById('num-one');
+var numTwo = document.getElementById('num-two');
+var addSum = document.getElementById('add-sum');
+
+numOne.addEventListener('input', add);
+numTwo.addEventListener('input', add);
+
+function add() {
+  var one = parseFloat(numOne.value) || 0;
+  var two = parseFloat(numTwo.value) || 0;
+  addSum.innerText = one + two ;
+}
+
+function sub() {
+  var one = parseFloat(numOne.value) || 0;
+  var two = parseFloat(numTwo.value) || 0;
+  subSum.innerText = one - two ;
+}
+
+function multiply() {
+  var one = parseFloat(numOne.value) || 0;
+  var two = parseFloat(numTwo.value) || 0;
+  mulSum.innerText = one * two ;
+}
+
+function divide() {
+  var one = parseFloat(numOne.value) || 0;
+  var two = parseFloat(numTwo.value) || 0;
+  divSum.innerText = one / two ;
+}
